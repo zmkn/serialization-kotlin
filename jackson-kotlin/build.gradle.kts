17146c15099d7b322646cda982eac88c8fe04919
hz <beijingren@vip.qq.com>
2025-12-15 14:09:04 +0800

    更新 jsckson 至 3.x 版本

--- jackson-kotlin/build.gradle.kts
+++ jackson-kotlin/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     api(libs.kotlin.time.jackson.module) // Kotlin time module for jackson
     api(libs.kotlinx.datetime.jackson.module) // Kotlinx datetime module for jackson
-    api(libs.jackson.databind) // JSON 序列化库
-    api(libs.jackson.module.kotlin) // jackson 支持 kotlin 模块库
+    api(platform(libs.tools.jackson.bom)) // Jackson Bom 物料库
+    api(libs.tools.jackson.databind) // JSON 序列化库
+    api(libs.tools.jackson.module.kotlin) // Jackson Kotlin 支持库
 }
--- jackson-kotlin/src/main/kotlin/com/zmkn/serialization/jackson/Jackson.kt
+++ jackson-kotlin/src/main/kotlin/com/zmkn/serialization/jackson/Jackson.kt
@@ -1,34 +1,37 @@
 package com.zmkn.serialization.jackson
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.databind.json.JsonMapper
-import com.fasterxml.jackson.module.kotlin.KotlinFeature
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.zmkn.jackson.module.datetime.DatetimeJacksonModule
 import com.zmkn.jackson.module.time.TimeJacksonModule
+import tools.jackson.core.json.JsonWriteFeature
+import tools.jackson.databind.DeserializationFeature
+import tools.jackson.databind.SerializationFeature
+import tools.jackson.databind.cfg.DateTimeFeature
+import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.kotlin.KotlinFeature
+import tools.jackson.module.kotlin.KotlinModule
 
 class Jackson(
     mapper: JsonMapper,
-    initializer: ObjectMapper.() -> Unit = {},
+    initializer: JsonMapper.Builder.() -> Unit = {},
     kotlinModuleInitializer: KotlinModule.Builder.() -> Unit = {},
 ) {
-    constructor(initializer: ObjectMapper.() -> Unit = {}, kotlinModuleInitializer: KotlinModule.Builder.() -> Unit = {}) : this(
-        Companion.jsonMapper, initializer, kotlinModuleInitializer
+    constructor(
+        initializer: JsonMapper.Builder.() -> Unit = {},
+        kotlinModuleInitializer: KotlinModule.Builder.() -> Unit = {}
+    ) : this(
+        Companion.jsonMapper,
+        initializer,
+        kotlinModuleInitializer
     )
 
     constructor() : this(Companion.jsonMapper)
 
-    val jsonMapper: JsonMapper = mapper.copy().apply {
+    val jsonMapper: JsonMapper = mapper.rebuild().apply {
         initializer()
-        registerModule(
-            defaultKotlinModuleBuilder.apply(kotlinModuleInitializer).build()
-        )
     }
+        .addModule(defaultKotlinModuleBuilder.apply(kotlinModuleInitializer).build())
+        .build()
 
     companion object {
         val defaultKotlinModuleBuilder = KotlinModule.Builder().apply {
@@ -37,25 +40,23 @@ class Jackson(
             enable(KotlinFeature.KotlinPropertyNameAsImplicitName) // 使用属性名进行序列化，确保与 Kotlin 的命名规则一致，避免因 getter 名称不同导致的问题
             enable(KotlinFeature.UseJavaDurationConversion) // 允许使用 JavaTimeModule 处理 Kotlin 的 Duration 类型，并需要在 getter 或 field 上声明 @JsonFormat 注解
         }
-        val jsonMapper = JsonMapper().apply {
+        val jsonMapper: JsonMapper = JsonMapper.builder().apply {
             // 注册 Kotlin 模块以支持 Kotlin 数据类
-            setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL) // 忽略所有值为 null 的属性
+            changeDefaultPropertyInclusion { incl -> incl.withValueInclusion(JsonInclude.Include.NON_NULL) } // 忽略所有值为 null 的属性
             // 配置序列化特性
             enable(SerializationFeature.INDENT_OUTPUT) // 在输出时美化打印JSON格式
+            enable(SerializationFeature.FAIL_ON_EMPTY_BEANS) // 使没有可序列化的属性时抛出异常
             enable(SerializationFeature.CLOSE_CLOSEABLE) // 使实现了 Closeable 接口的对象（例如文件流或网络连接）都会被自动关闭
             enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS) // 使 Map 类型的对象在序列化为 JSON 时，条目会根据键进行排序
             enable(SerializationFeature.USE_EQUALITY_FOR_OBJECT_ID) // 使 Jackson 在处理对象 ID 引用时使用 equals() 方法来比较对象
-            disable(SerializationFeature.FAIL_ON_EMPTY_BEANS) // 使没有可序列化的属性时不抛出异常
-            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // 使日期类型不会被序列化为时间戳
+            disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS) // 使日期类型不会被序列化为时间戳
             // 配置反序列化特性
-            enable(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS) // 如果尝试将数字作为枚举类型的值，则抛出异常
             enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES) // 如果尝试将 JSON 中的 null 值赋给 Java 的基本数据类型（如 int、boolean），则抛出异常
             disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES) // 使 JSON 中包含目标类中不存在的字段时不抛出异常
             // 配置解析器 (JsonParser) 和生成器 (JsonGenerator) 的行为
-            configure(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION, true) // 在错误报告中包含原始输入源的信息
-            configure(JsonGenerator.Feature.COMBINE_UNICODE_SURROGATES_IN_UTF8, true) // 在 UTF-8 编码输出中，将 Unicode 代理对（surrogate pairs）组合成单个字符
+            configure(JsonWriteFeature.COMBINE_UNICODE_SURROGATES_IN_UTF8, true) // 在 UTF-8 编码输出中，将 Unicode 代理对（surrogate pairs）组合成单个字符
         }.also {
-            it.registerModule(defaultKotlinModuleBuilder.build()).registerModule(DatetimeJacksonModule.all).registerModule(TimeJacksonModule.all)
-        }
+            it.addModule(defaultKotlinModuleBuilder.build()).addModule(DatetimeJacksonModule.all).addModule(TimeJacksonModule.all)
+        }.build()
     }
 }
